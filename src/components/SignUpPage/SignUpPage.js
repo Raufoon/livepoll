580892bb5cbd2640c7b0be9bc0159f53b2d302b2
Raufoon <minhazraufooncsedu@gmail.com>
2018-09-13 00:48:29 +0600

    basic signup page implemented

--- src/components/SignUpPage/SignUpPage.js
+++ src/components/SignUpPage/SignUpPage.js
@@ -1,9 +1,20 @@
 import React from 'react';
 
+import './SignUpPage.css';
+
 const SignUpPage = () => {
   return (
-    <div>
-      landed in signup page
+    <div className='signup-page'>
+      <div className='signup-page-content'>
+        <label className='webapp-label'>livepoll</label><br/><br/>
+        <input className='webapp-ep-input' type='email' placeholder='email or phone'/><br/>
+        <input className='webapp-ep-input' type='password' placeholder='password'/><br/>
+        <button className='webapp-ep-input'>Log In</button>
+        <div>
+          <button className='webapp-shortcut-signin-btn'>Google</button>
+          <button className='webapp-shortcut-signin-btn'>Facebook</button>
+        </div>
+      </div>
     </div>
   )
 };
