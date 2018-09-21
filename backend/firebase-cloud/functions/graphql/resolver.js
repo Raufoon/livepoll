7f3fbb1645fb4d4d56b83e49039dfd744c3acba9
Raufoon <minhazraufooncsedu@gmail.com>
2018-09-21 23:30:27 +0600

    backend: livepoll graphql schema added

--- backend/firebase-cloud/functions/graphql/resolver.js
+++ backend/firebase-cloud/functions/graphql/resolver.js
@@ -1,20 +1,62 @@
 const DB = require('../firebase-database/realtime-database');
+const uuidv1 = require('uuid/v1');
 
 const resolvers = {
   User: {
     name: obj => obj.name,
     dob: obj => obj.dob,
   },
+  PollPrivacy: {
+    public: 'pb',
+    private: 'pr',
+  },
+  PollItemFormat: {
+    text: 't',
+    textWithImage: 'ti',
+    textWithImages: 'ti8',
+    textWithVideo: 'tv',
+  },
+  VoteType: {
+    tickVote: 't',
+    numberVote010: 'n10',
+    numberVote0100: 'n100',
+  },
+  WhoCanAddItem: {
+    anyone: 'a',
+    onlyCreator: 'c',
+  },
+  LivepollSettings: {
+    title: obj => obj.title,
+    startDatetime: obj => obj.startDatetime,
+    endDatetime: obj => obj.endDatetime,
+    privacy: obj => obj.privacy,
+    voteType: obj => obj.voteType,
+    format: obj => obj.format,
+    whoCanAddItem: obj => obj.whoCanAddItem,
+  },
+  Livepoll: {
+    id: obj => obj.id,
+    settings: obj => obj.settings,
+  },
+
   Query: {
     user: (_, { id }) => DB.read(`/users/${id}`),
     users: () => DB.readList('/users'),
   },
+
   Mutation: {
     createUser: (_, { id, name, dob }) => {
       return DB.write(`/users/${id}`, {
         id,
         name,
         dob
+      });
+    },
+    publishLivepoll: (_, { settings }) => {
+      const id = uuidv1();
+      return DB.write(`/polls/${id}`, {
+        id,
+        settings
       });
     }
   }
--- backend/firebase-cloud/functions/graphql/schema.js
+++ backend/firebase-cloud/functions/graphql/schema.js
@@ -6,12 +6,48 @@ const typeDefs = gql`
     name: String
     dob: String
   }
+
+  enum PollPrivacy {
+    public
+    private
+  }
+  enum PollItemFormat {
+    text
+    textWithImage
+    textWithImages
+    textWithVideo
+  }
+  enum VoteType {
+    tickVote
+    numberVote010
+    numberVote0100
+  }
+  enum WhoCanAddItem {
+    anyone
+    onlyCreator
+  }
+  type LivepollSettings {
+    title: String!
+    startDatetime: String!
+    endDatetime: String!
+    privacy: PollPrivacy!
+    voteType: VoteType!
+    format: PollItemFormat!
+    whoCanAddItem: WhoCanAddItem!
+  }
+  type Livepoll {
+    id: ID!
+    settings: LivepollSettings!
+  }
+
   type Query {
     user(id: ID!): User
     users: [User]!
   }
+
   type Mutation {
     createUser(id: ID!, name: String, dob: String): User
+    publishLivepoll(settings: LivepollSettings!): Livepoll
   }
 `;
 
