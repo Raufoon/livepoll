0864279600b41db44e6aaae4e3cfce3825566b37
Raufoon <minhazraufooncsedu@gmail.com>
2019-04-12 15:37:05 +0600

    redesign: livepoll page

--- src/components/LivepollPage/LivepollItemList/item-components/TextPollItem.js
+++ src/components/LivepollPage/LivepollItemList/item-components/TextPollItem.js
@@ -1,131 +1,88 @@
 import React from 'react'
-import MediaQuery from 'react-responsive';
 import { withStyles } from '@material-ui/core/styles';
 import PropTypes from 'prop-types'
-import Card from '@material-ui/core/Card';
-import CardHeader from '@material-ui/core/CardHeader'
-import Button from '@material-ui/core/Button';
-import Avatar from '@material-ui/core/Avatar'
-import Typography from "@material-ui/core/Typography/Typography";
-import StarIcon from '@material-ui/icons/Star';
-import Loadable from 'react-loadable';
+import ImageButton from "../../../utils/ImageButton";
+import {STAR_URL} from "../../../../constants/livepoll-constants";
 
-import './TextItem.css'
-import {FIRST_TROPHY_IMG_URL} from '../../../../constants/livepoll-constants';
-import ModalOpenerButton from "../../../utils/modal-openers/ModalOpenerButton/ModalOpenerButton";
-import VoteCountChip from "../../../utils/VoteCountChip/VoteCountChip";
-import LPLoader from "../../../loaders/LPLoader";
 
-const LivepollItemVoterList = Loadable({
-  loader: ()=>import('../../LivepollItemVoterList/LivepollItemVoterList'),
-  loading: LPLoader,
+const styles = () => ({
+  voteButton: {
+    textTransform: 'uppercase',
+    fontWeight: 'normal',
+  },
+  voteCounter: {
+    fontSize: 'x-small',
+    fontWeight: 'bold',
+    color: '#222',
+    textTransform: 'uppercase'
+  },
+  itemSerial: {
+    color: '#19564c',
+    fontWeight: 'bold',
+    fontSize: 'large',
+  },
+  item: {
+    fontFamily: 'Comfortaa',
+    marginBottom: '10px',
+    backgroundColor: '#fbfbfb',
+    border: '1px solid lightgrey',
+    borderRadius: '5px'
+  },
+  itemText: {
+    fontSize: 'large',
+    height: '100%',
+    marginLeft: 10
+  }
 });
 
-const TextPollItemAvatar = props => {
-  if (props.isFirst) {
-    return (
-      <Avatar className={'avatar-first-item avatar-first-item-resp'} src={FIRST_TROPHY_IMG_URL}>
-        {props.index}
-      </Avatar>
-    )
-  } else {
-    return (
-      <Avatar className={'avatar-regular'}>
-        {props.index}
-      </Avatar>
-    )
-  }
-};
+const TextPollItem = props => {
+  const {
+    classes,
+    item,
+    index,
+    isAlreadyVoted
+  } = props;
 
-const VoteButton = (props) => {
-  let style = {...props.style};
-  style.color = props.isAlreadyVoted ? '#e5abab':'#80af71';
+  const serialStyle = {
+    backgroundColor: '#eeeeee',
+  };
+  const voteButtonStyle = {
+    color: 'black',
+  };
+
+  if (index === 1) {
+    serialStyle.backgroundColor = '#f7c741';
+    serialStyle.color = 'white';
+  }
+  else if (index === 2) {
+    serialStyle.backgroundColor = '#f7bf8f';
+    serialStyle.color = 'white';
+  }
+
+  if (isAlreadyVoted) {
+    voteButtonStyle.color = 'green';
+    voteButtonStyle.fontWeight = 'bold';
+  }
+
   return (
-    <Button
-      className={props.className}
-      style={style}
-      size={'small'}
-      onClick={props.vote}>
-      <StarIcon/>
-      &nbsp;
-      {props.isAlreadyVoted ? 'Unvote':'Vote'}
-    </Button>
+    <div className={`pure-g ${classes.item}`}>
+      <div className={`pure-u-2-24 ${classes.itemSerial} vert-cent`} style={serialStyle}>{index}</div>
+      <div className="pure-u-22-24">
+        <div className={'pure-g mg5'}>
+          <label className={`pure-u-1-1 ${classes.itemText}`}>{item.content.text}</label>
+          <div className={`tac pure-u-1-1 ${classes.voteCounter}`}>
+            <ImageButton src={STAR_URL} iconHeight={10} text={`${ props.item.voteCount } votes`}/>
+            <button className={`pure-button fr ${classes.voteButton}`} onClick={props.vote} style={voteButtonStyle}>
+              {isAlreadyVoted ? 'Unvote':'Vote'}
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 };
 
-const VoteCounter = props => {
-  if (props.hideVotes) return <div/>;
-  let voteCount = props.item.voteCount;
-  if (props.isPercentView) {
-    voteCount = parseInt((voteCount / props.totalVotes)*10000, 10);
-    voteCount /= 100;
-    voteCount += '%';
-  }
-  if (props.showVoters) {
-    return (
-      <ModalOpenerButton
-        className={props.className}
-        ModalComponent={LivepollItemVoterList}
-        childProps={{
-          pollId: props.pollId,
-          itemId: props.item.id,
-          voterList: props.item.voterIds || [],
-        }}
-        OpenerComponent={Typography}
-        dontHideOpener={true}
-        openerComponentProps={{variant: 'body2'}}
-      >
-        <VoteCountChip count={voteCount} short={true}/>
-      </ModalOpenerButton>
-    )
-  }
-  return <VoteCountChip className={props.className} short={true} count={voteCount}/>;
-};
-
-const styles = (theme) => ({
-  voteButton: {
-    textTransform: 'none',
-    padding: 0,
-  },
-  voteCounter: {
-    textTransform: 'none',
-    padding: 0,
-    color: '#ae7b09',
-    fontSize: 'large'
-  }
-});
-
-const Title = (props) => (
-  <React.Fragment>
-    <MediaQuery minWidth={800}>
-      <Typography variant={props.isFirst ? 'h4' : 'h5'} gutterBottom>{props.text}</Typography>
-    </MediaQuery>
-    <MediaQuery maxWidth={799}>
-      <Typography variant={'h5'} gutterBottom>{props.text}</Typography>
-    </MediaQuery>
-  </React.Fragment>
-);
-
-const TextPollItem = props => (
-  <Card className={'text-item text-item-resp'}>
-    <CardHeader
-      avatar={<TextPollItemAvatar isFirst={props.isFirst} index={props.index}/>}
-      action={<VoteCounter {...props} className={props.classes.voteCounter}/>}
-      title={<Title isFirst={props.isFirst} text={props.item.content.text}/>}
-      subheader={
-        <div>
-          {!props.voteDisabled && <VoteButton {...props} className={props.classes.voteButton}/>}
-        </div>
-      }
-      subheaderTypographyProps={{
-        variant: props.isFirst ? 'subtitle1' : 'caption'
-      }}
-    />
-  </Card>
-);
-
 TextPollItem.propTypes = {
-  isFirst: PropTypes.bool,
   isAlreadyVoted: PropTypes.bool,
   voteDisabled: PropTypes.bool,
   vote: PropTypes.func,
