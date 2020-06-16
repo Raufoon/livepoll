da9dfe1e8184a7c50ade4510349f75b31449036f
Minhaz Raufoon <minhaz.raufoon.1567@gmail.com>
2020-06-16 17:33:26 +0200

    Write database on new user login

--- server/functions/index.js
+++ server/functions/index.js
@@ -2,6 +2,7 @@ const functions = require('firebase-functions')
 const express = require('express')
 const cors = require('cors')({origin: true})
 const admin = require('firebase-admin')
+const db = require('./realtimeDb')
 
 if (process.env.NODE_ENV === 'production') {
   admin.initializeApp()  
@@ -14,10 +15,16 @@ else {
   })
 }
 
+functions.auth.user().onCreate(newUser => {
+  const {uid, displayName} = newUser.data
+  db.write(`/users/${uid}`, {
+    name: displayName,
+    id: uid
+  })
+})
+
 const app = express()
-
 app.use(cors)
-
 app.use('/', require('./graphql'))
 
 exports.graphql_v_2_0_0 = functions.https.onRequest(app)
