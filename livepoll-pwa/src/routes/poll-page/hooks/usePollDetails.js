f20ec4d2303ff31b2794a73c5d358c4aa42b3b89
Minhaz Raufoon <minhaz.raufoon.1567@gmail.com>
2020-06-19 18:04:45 +0200

    Fetch poll details successfully from server to poll page (pwa)

--- livepoll-pwa/src/routes/poll-page/hooks/usePollDetails.js
+++ livepoll-pwa/src/routes/poll-page/hooks/usePollDetails.js
@@ -1,7 +1,16 @@
 import {useSelector, useDispatch} from 'react-redux'
+import {useEffect} from 'react'
+import {actionFetchPollDetails} from '../../../state-management/actions/poll-actions'
 
 export default function usePollDetails(pollId) {
-  const details = useSelector(state => state.polls[pollId])
+  const pollDetails = useSelector(state => state.polls[pollId])
+  const dispatch = useDispatch()
 
-  return details
+  useEffect(function() {
+    if (!pollDetails) {
+      dispatch(actionFetchPollDetails(pollId))
+    }
+  }, [pollId])
+
+  return pollDetails
 }
