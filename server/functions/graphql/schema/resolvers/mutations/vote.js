da73323f5c6f4a7f6acae21146ac6fa2c37a5437
Minhaz Raufoon <minhaz.raufoon.1567@gmail.com>
2020-06-21 00:36:29 +0200

    Prevent duplicate vote and enable user to change their vote (server)

--- server/functions/graphql/schema/resolvers/mutations/vote.js
+++ server/functions/graphql/schema/resolvers/mutations/vote.js
@@ -5,9 +5,18 @@ module.exports = async function vote(_, args, context) {
 
   try {
     const voterId = await context.getAuthUserId()
+    const alreadyVotedItemId = await db.read(`edges/voter_poll_item/${voterId}/${pollId}`)
+
+    if (itemId === alreadyVotedItemId) {
+      throw new Error('You have already voted for this item')
+    }
+    else if (!!alreadyVotedItemId) {
+      await db.transaction(`edges/poll_item/${pollId}/${alreadyVotedItemId}`, oldScore => oldScore - voteValue)
+    }
+
     await db.transaction(`edges/poll_item/${pollId}/${itemId}`, oldScore => oldScore + voteValue)
-    // TODO: if voter list is visible, save an edge item_voters/ITEM_ID/VOTER_ID = true
     await db.write(`edges/voter_poll_item/${voterId}/${pollId}`, itemId)
+    // TODO: if voter list is visible, save an edge item_voters/ITEM_ID/VOTER_ID = true
   }
   catch(err) {
     return Promise.reject(err)
