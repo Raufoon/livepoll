8908e244009178114a389f45c6aa8f9f9918f36d
Minhaz Raufoon <minhaz.raufoon.1567@gmail.com>
2020-06-21 03:53:30 +0200

    Fix vote api (server)

--- server/functions/graphql/schema/resolvers/mutations/vote.js
+++ server/functions/graphql/schema/resolvers/mutations/vote.js
@@ -10,17 +10,20 @@ module.exports = async function vote(_, args, context) {
     if (itemId === alreadyVotedItemId) {
       throw new Error('You have already voted for this item')
     }
-    else if (!!alreadyVotedItemId) {
-      await db.transaction(`edges/poll_item/${pollId}/${alreadyVotedItemId}`, oldScore => oldScore - voteValue)
-    }
 
     await db.transaction(`edges/poll_item/${pollId}/${itemId}`, oldScore => oldScore + voteValue)
     await db.write(`edges/voter_poll_item/${voterId}/${pollId}`, itemId)
     
     const currentScore = await db.read(`edges/poll_item/${pollId}/${itemId}`)
     await db.write(`items/${itemId}/score`, currentScore)
-    
+
     // TODO: if voter list is visible, save an edge item_voters/ITEM_ID/VOTER_ID = true
+
+    if (!!alreadyVotedItemId) {
+      await db.transaction(`edges/poll_item/${pollId}/${alreadyVotedItemId}`, oldScore => oldScore - voteValue)
+      const scoreOfCancelledItem = await db.read(`edges/poll_item/${pollId}/${alreadyVotedItemId}`)
+      await db.write(`items/${alreadyVotedItemId}/score`, scoreOfCancelledItem)
+    }
   }
   catch(err) {
     return Promise.reject(err)
