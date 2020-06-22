cef8941ab3829809f45949b96d4fb47e727ec510
Minhaz Raufoon <minhaz.raufoon.1567@gmail.com>
2020-06-22 04:06:14 +0200

    Resize images before uploading (server)

--- server/functions/routers/file-upload.js
+++ server/functions/routers/file-upload.js
@@ -5,6 +5,7 @@ const fs = require('fs')
 const path = require('path')
 const os = require('os')
 const admin = require('firebase-admin')
+const sharp = require('sharp')
 
 router.post('/avatar', async function (request, response) {
   const {uid} = await admin.auth().verifyIdToken(request.headers.authorization)
@@ -16,12 +17,12 @@ router.post('/avatar', async function (request, response) {
   
   const busboy = new Busboy({headers: request.headers})
   const tmpdir = os.tmpdir()
-  let upload;
+  let rawImgPath;
   let fileWrite;
   
   busboy.on('file', (fieldname, file, filename) => {
-    upload = path.join(tmpdir, filename)
-    const writeStream = fs.createWriteStream(upload)
+    rawImgPath = path.join(tmpdir, filename)
+    const writeStream = fs.createWriteStream(rawImgPath)
     file.pipe(writeStream)
 
     fileWrite = new Promise(function(resolve, reject) {
@@ -35,10 +36,17 @@ router.post('/avatar', async function (request, response) {
   
   busboy.on('finish', async function() {
     await fileWrite
-    const bucket = admin.storage().bucket()
-    const destination = `avatars/${uid}_${Date.now()}.${path.extname(upload)}`
-    await bucket.upload(upload, {destination})
-    fs.unlinkSync(upload)
+    const outputImgName = `${uid}_${Date.now()}.${path.extname(rawImgPath)}`
+
+    const resizedImgPath = path.join(tmpdir, outputImgName)
+
+    await sharp(rawImgPath).resize(256, null).toFile(resizedImgPath);
+
+    const destination = `avatars/${outputImgName}`
+    await admin.storage().bucket().upload(resizedImgPath, {destination})
+
+    fs.unlinkSync(rawImgPath)
+    fs.unlinkSync(resizedImgPath)
 
     const uploadedImgUrl = `https://storage.googleapis.com/lllivepolll.appspot.com/${destination}`;
     response.send(JSON.stringify({uploadedImgUrl}))
--- server/index.js
+++ server/index.js
@@ -1,10 +1,13 @@
 const express = require('express')
+const multer = require('multer')
+const cors = require('cors')({origin: true})
+const fileUpload = require('./functions/routers/file-upload')
 
 const app = express()
 
-app.get('/', (req, res) => {
-  res.sendFile('test.html', {root: __dirname})
-})
+app.use(cors)
+
+app.use('/upload', fileUpload)
 
 app.listen(9000, function () {
   console.log("Listening at 9000")
