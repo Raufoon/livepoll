69a113019b5522ef799a5ae21580b5e774e35941
Minhaz Raufoon <minhaz.raufoon.1567@gmail.com>
2020-06-23 22:21:10 +0200

    Fix (server)

--- server/functions/graphql/schema/resolvers/queries/which-did-i-vote.js
+++ server/functions/graphql/schema/resolvers/queries/which-did-i-vote.js
@@ -4,7 +4,7 @@ module.exports = async function whichDidIVote(_, args, context) {
   const {pollId} = context
   try {
     const voterId = await context.getAuthUserId()
-    const votedItemId = await db.read(`/voter_poll_item/${voterId}/${pollId}`)
+    const votedItemId = await db.read(`edges/voter_poll_item/${voterId}/${pollId}`)
     return Promise.resolve(votedItemId)
   }
   catch(err) {
