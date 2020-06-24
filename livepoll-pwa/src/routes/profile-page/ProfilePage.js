d97e4662e729d51dfa92937697c86aae9b008168
Minhaz Raufoon <minhaz.raufoon.1567@gmail.com>
2020-06-24 19:31:43 +0200

    Beautify the profile page (pwa)

--- livepoll-pwa/src/routes/profile-page/ProfilePage.js
+++ livepoll-pwa/src/routes/profile-page/ProfilePage.js
@@ -1,6 +1,5 @@
-import React, {useContext} from 'react'
+import React from 'react'
 import {useParams} from 'react-router-dom'
-import AuthContext from '../../contexts/AuthContext'
 import useProfileDetails from './hooks/useProfileDetails'
 import ProfileEditor from './components/profile-editor/ProfileEditor'
 import ProfileSummary from './components/profile-summary/ProfileSummary'
@@ -11,8 +10,7 @@ export default function ProfilePage() {
 
   const {id} = useParams()
   const details = useProfileDetails(id)
-  const authUser = useContext(AuthContext)
-  const isMyProfile = authUser.getUid() === id
+  
 
   return (
     <div className='ProfilePage'>
--- livepoll-pwa/src/routes/profile-page/components/profile-summary/ProfileSummary.js
+++ livepoll-pwa/src/routes/profile-page/components/profile-summary/ProfileSummary.js
@@ -1,17 +1,37 @@
-import React from 'react'
+import React, {useContext} from 'react'
 import avatarIcon from '../../../poll-page/components/user-badge/images/default-user.png'
+import { NavLink } from 'react-router-dom'
+import IconButton from '../../../../components/icon-button/IconButton'
+import AuthContext from '../../../../contexts/AuthContext'
+import activityIcon from './images/activity.png'
+import pollsIcon from './images/polls.png'
+import settingsIcon from './images/settings.png'
 import './style.css'
 
 export default function ProfileSummary(props) {
+  const authUser = useContext(AuthContext)
+
   const {className, details} = props
   if (!details) return 'Loading...'
 
-  const {avatar, name, id, dob} = details
+  const {avatar, name, id} = details
+  const isMyProfile = authUser.getUid() === id
 
   return (
     <div className={`ProfileSummary ${className}`}>
       <div className='userPropic' style={{backgroundImage: `url(${avatar || avatarIcon})`}}/>
       <label className='userfullname'>{name}</label>
+      <label>livepoll user</label>
+
+      <nav>
+        <IconButton to='/activity' iconClass='icon' iconUrl={activityIcon}>Activity</IconButton>
+        <IconButton to='/polls' iconClass='icon' iconUrl={pollsIcon}>Polls</IconButton>
+        {
+          isMyProfile && <>
+            <IconButton to='/settings' iconClass='icon' iconUrl={settingsIcon}>settings</IconButton>
+          </>
+        }
+      </nav>
     </div>
   )
 }
