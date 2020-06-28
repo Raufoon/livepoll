e31b6e23e2aec664706c5b38df913f3fc5429c8a
Minhaz Raufoon <minhaz.raufoon.1567@gmail.com>
2020-06-28 05:37:52 +0200

    Make the profile page responsive (pwa)

--- livepoll-pwa/src/routes/profile-page/ProfilePage.js
+++ livepoll-pwa/src/routes/profile-page/ProfilePage.js
@@ -1,9 +1,14 @@
-import React from 'react'
+import React,{useContext} from 'react'
 import {useParams, Switch, Route} from 'react-router-dom'
 import useProfileDetails from './hooks/useProfileDetails'
 import ProfileSummary from './components/profile-summary/ProfileSummary'
 import ProfileSettings from './routes/profile-settings/ProfileSettings'
 import Responsive from '../../components/responsive/Responsive'
+import IconButton from '../../components/icon-button/IconButton'
+import activityIcon from './images/activity.png'
+import pollsIcon from './images/polls.png'
+import settingsIcon from './images/settings.png'
+import AuthContext from '../../contexts/AuthContext'
 import './style.css'
 
 export default function ProfilePage(props) {
@@ -11,13 +16,38 @@ export default function ProfilePage(props) {
 
   const {id} = useParams()
   const details = useProfileDetails(id)
+  const authUser = useContext(AuthContext)
   const {match} = props
+  const isMyProfile = authUser.getUid() === id
+
+  const navButtons = <>
+    <IconButton 
+      to={`${match.url}/activity`} 
+      activeClassName='activeNavBtn' 
+      iconClass='icon' 
+      iconUrl={activityIcon}>Activity</IconButton>   
+    <IconButton 
+      to={`${match.url}/polls`} 
+      activeClassName='activeNavBtn' 
+      iconClass='icon' 
+      iconUrl={pollsIcon}>Polls</IconButton> 
+    {
+      isMyProfile && <IconButton 
+        to={`${match.url}/settings`} 
+        iconClass='icon' 
+        activeClassName='activeNavBtn' 
+        iconUrl={settingsIcon}>Settings</IconButton>
+    }
+  </>
 
   return (
     <div className='ProfilePage'>
       <Responsive screens={['M', 'L']}>
         <div className='leftSect'>
           <ProfileSummary className='profileSummary' details={details}/>
+          <div className="profileNav">
+            {navButtons}
+          </div>
         </div>
 
         <div className='rightSect'>
@@ -28,7 +58,15 @@ export default function ProfilePage(props) {
       </Responsive>
 
       <Responsive screens={['S']}>
-        mobile screen :)
+        <div className="mobile">
+          <ProfileSummary className='profileSummary' details={details}/>
+          <div className="profileNav">
+            {navButtons}
+          </div>
+          <Switch>
+            <Route path={`${match.path}/settings`} component={ProfileSettings}/>
+          </Switch>
+        </div>
       </Responsive>      
     </div>
   )
--- livepoll-pwa/src/routes/profile-page/components/profile-summary/ProfileSummary.js
+++ livepoll-pwa/src/routes/profile-page/components/profile-summary/ProfileSummary.js
@@ -1,50 +1,18 @@
-import React, {useContext} from 'react'
+import React from 'react'
 import avatarIcon from '../../../poll-page/components/user-badge/images/default-user.png'
-import { useRouteMatch } from 'react-router-dom'
-import IconButton from '../../../../components/icon-button/IconButton'
-import AuthContext from '../../../../contexts/AuthContext'
-import activityIcon from './images/activity.png'
-import pollsIcon from './images/polls.png'
-import settingsIcon from './images/settings.png'
 import './style.css'
 
 export default function ProfileSummary(props) {
-  const authUser = useContext(AuthContext)
-  const match = useRouteMatch()
-
   const {className, details} = props
   if (!details) return 'Loading...'
 
   const {avatar, name, id} = details
-  const isMyProfile = authUser.getUid() === id
 
   return (
     <div className={`ProfileSummary ${className}`}>
       <div className='userPropic' style={{backgroundImage: `url(${avatar || avatarIcon})`}}/>
       <label className='userfullname'>{name}</label>
       <label className='subtitle'>livepoll user</label>
-
-      <nav>
-        <IconButton 
-          to={`${match.url}/activity`} 
-          activeClassName='activeNavBtn' 
-          iconClass='icon' 
-          iconUrl={activityIcon}>Activity</IconButton>
-        
-        <IconButton 
-          to={`${match.url}/polls`} 
-          activeClassName='activeNavBtn' 
-          iconClass='icon' 
-          iconUrl={pollsIcon}>Polls</IconButton>
-        
-        {
-          isMyProfile && <IconButton 
-            to={`${match.url}/settings`} 
-            iconClass='icon' 
-            activeClassName='activeNavBtn' 
-            iconUrl={settingsIcon}>Settings</IconButton>
-        }
-      </nav>
     </div>
   )
 }
