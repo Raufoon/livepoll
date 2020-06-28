ce5170c26cc3aedb64712dd0996bce79f10c358f
Minhaz Raufoon <minhaz.raufoon.1567@gmail.com>
2020-06-28 18:47:31 +0200

    Make image item views responsive (pwa)

--- livepoll-pwa/src/routes/poll-page/components/items-panel/components/image-caption-item/ImageCaptionItem.js
+++ livepoll-pwa/src/routes/poll-page/components/items-panel/components/image-caption-item/ImageCaptionItem.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Responsive from '../../../../../../components/responsive/Responsive'
 import VoteLabel from '../../../../../../components/vote-label/VoteLabel'
 import ProgBar from '../../../../../../components/prog-bar/ProgBar'
 import './style.css'
@@ -8,22 +9,26 @@ export default function ImageCaptionItem(props) {
   const {text, id, imgUrl, score} = item
 
   const percent = Math.ceil((score *100) / (totalVotes||1))
-  const progbar = <ProgBar className='progBar' value={percent || 1}/>
   const voteLabel = shouldShowVoters ? (
-    <VoteLabel count={score} percent={percent} onClick={() => displayVoterList(item.id)}/>
+    <VoteLabel className="voteLabel" count={score} percent={percent} onClick={() => displayVoterList(item.id)}/>
   ):(
-    <VoteLabel count={score} percent={percent}/>
+    <VoteLabel className="voteLabel" count={score} percent={percent}/>
   )
-  const voteCheckbox = <input type='checkbox' checked={isVotedByMe} onChange={() => vote(id)}/>
 
   return(
     <div className='ImageCaptionItem'>
       <div className="topRow">
         <label className="caption">{1+position}. {text}</label>
-        {voteCheckbox}
+        <input type='checkbox' checked={isVotedByMe} onChange={() => vote(id)}/>
       </div>
-      <img className='image' src={imgUrl} alt="item"/>
+      <Responsive screens={['M', 'L']}>
+        <img className='image' src={imgUrl} alt="item"/>
+      </Responsive>
+      <Responsive screens={['S']}>
+        <img className='image mobile' src={imgUrl} alt="item"/>
+      </Responsive>
       <ProgBar className='progBar' value={percent || 1}/>
+      {voteLabel}
     </div>
   )
 }
