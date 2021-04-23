d646ca29462c580305d944e9e6eedfce769e6893
Raufoon <minhaz.raufoon.1567@gmail.com>
2021-04-23 12:18:34 +0200

    Add docker support

--- livepoll-pwa/src/services/network.js
+++ livepoll-pwa/src/services/network.js
@@ -1,39 +1,43 @@
-import {get, post} from 'axios'
-import {getAuthIDToken} from './auth'
+import { get, post } from 'axios'
+import { getAuthIDToken } from './auth'
 
 // const DEV_SERVER = 'http://localhost:5000/lllivepolll/us-central1/server'
-const SERVER = 'https://us-central1-lllivepolll.cloudfunctions.net/server'
+const SERVER = 'http://localhost:9000'
 
 export async function graphQlSecureQuery(query) {
   const idToken = await getAuthIDToken()
   const headers = {
-    'Authorization': idToken
+    Authorization: idToken,
   }
-  return fetch(`${SERVER}/graphql?query=${query}`, {headers})
-    .then(response => response.json())
+  return fetch(`${SERVER}/graphql?query=${query}`, {
+    headers,
+  }).then((response) => response.json())
   //return get(`${SERVER}/graphql?query=${query}`, {headers}).then(response => response.data)
 }
 
 export function graphQlQuery(query) {
-  return fetch(`${SERVER}/graphql?query=${query}`)
-    .then(response => response.json())
+  return fetch(`${SERVER}/graphql?query=${query}`).then((response) =>
+    response.json()
+  )
   //return get(`${SERVER}/graphql?query=${query}`).then(response => response.data)
 }
 
 export async function graphQlMutation(query, variables) {
   const idToken = await getAuthIDToken()
   const headers = {
-    'Authorization': idToken
+    Authorization: idToken,
   }
-  return post(`${SERVER}/graphql`, {query, variables}, {headers})
-    .then(response => response.data)
+  return post(`${SERVER}/graphql`, { query, variables }, { headers }).then(
+    (response) => response.data
+  )
 }
 
 export async function doSecurePostRequest(path, data) {
   const idToken = await getAuthIDToken()
   const headers = {
-    'Authorization': idToken
+    Authorization: idToken,
   }
-  return post(`${SERVER}/${path}`, data, {headers})
-    .then(response => response.data)
+  return post(`${SERVER}/${path}`, data, { headers }).then(
+    (response) => response.data
+  )
 }
--- server/functions/index.js
+++ server/functions/index.js
@@ -1,32 +1,40 @@
 const functions = require('firebase-functions')
 const express = require('express')
-const cors = require('cors')({origin: true})
+const cors = require('cors')({ origin: true })
 const admin = require('firebase-admin')
 const db = require('./realtimeDb')
-const {types, query, mutation} = require('./graphql')
+const { types, query, mutation } = require('./graphql')
 const graphqlHTTP = require('express-graphql')
-const {GraphQLSchema} = require('graphql')
+const { GraphQLSchema } = require('graphql')
 const fileUpload = require('./routers/file-upload')
-const dummyServer = require('./routers/dummy-server')
 const bodyParser = require('body-parser')
 
+admin.initializeApp({
+  apiKey: 'AIzaSyBQSHVAGfc3pCsx6VBM-I0sMiT0r17Z5Ek',
+  authDomain: 'lllivepolll.firebaseapp.com',
+  databaseURL: 'https://lllivepolll.firebaseio.com',
+  projectId: 'lllivepolll',
+  storageBucket: 'lllivepolll.appspot.com',
+  messagingSenderId: '1045079837725',
+})
+
+/*
 if (process.env.NODE_ENV === 'production') {
-  admin.initializeApp()  
-}
-else {
-  const serviceAccount = require("./lllivepolll-411ae6bac230.json")
+} else {
+  const serviceAccount = require('./lllivepolll-411ae6bac230.json')
   admin.initializeApp({
-      credential: admin.credential.cert(serviceAccount),
-      databaseURL: "https://lllivepolll.firebaseio.com",
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: 'https://lllivepolll.firebaseio.com',
   })
 }
+*/
 
-exports.setUserProfile = functions.auth.user().onCreate(newUser => {
-  const {uid, displayName, photoURL} = newUser
+exports.setUserProfile = functions.auth.user().onCreate((newUser) => {
+  const { uid, displayName, photoURL } = newUser
   db.write(`/users/${uid}`, {
     name: displayName,
     id: uid,
-    avatar: photoURL
+    avatar: photoURL,
   })
 })
 
@@ -36,20 +44,27 @@ app.use(cors)
 
 app.use('/upload', fileUpload)
 
-app.use('/graphql', graphqlHTTP(function(request, response) {
-  return {
-    schema: new GraphQLSchema({...types, query, mutation}),
-    context: {
-      getAuthUserId: async function() {
-        const {uid} = await admin.auth().verifyIdToken(request.headers.authorization)
-        return uid
-      }
-    },
-    graphiql: process.env.NODE_ENV === 'development'
-  }
-}))
+app.use(
+  '/graphql',
+  graphqlHTTP(function (request, response) {
+    return {
+      schema: new GraphQLSchema({ ...types, query, mutation }),
+      context: {
+        getAuthUserId: async function () {
+          const { uid } = await admin
+            .auth()
+            .verifyIdToken(request.headers.authorization)
+          return uid
+        },
+      },
+      graphiql: process.env.NODE_ENV === 'development',
+    }
+  })
+)
 
-app.use(bodyParser.urlencoded({extended: true}))
-app.use('/dummy', dummyServer)
+app.use(bodyParser.urlencoded({ extended: true }))
 
-exports.server = functions.https.onRequest(app)
+app.listen(9000, () => console.log('GraphQL server started on port 9000'))
+
+// For firebase cloud function
+//exports.server = functions.https.onRequest(app)
